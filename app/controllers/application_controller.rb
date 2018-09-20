e2d56d4291b808b5e7065ea0ac2b9ea40bcc43e3
ElvisLYC <yinchuang94@gmail.com>
2018-09-20 11:50:50 +0800

    First Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,5 @@
 class ApplicationController < ActionController::Base
-  # include Clearance::Controller
+  include Clearance::Controller
 
-  def new
-    @user = User.new
-  end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      sign_in @user
-      redirect_back_or url_after_create
-    else
-      render template: "users/new"
-    end
-end
-
-private
-  def user_params
-    params.require(:user).permit(:email, :password, :full_name)
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_09_19_144532) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "full_name", null: false
     t.string "email", null: false
     t.string "encrypted_password", limit: 128, null: false
     t.string "confirmation_token", limit: 128
