3b4a4bd369d7e8a3a4ea8d798cacf004dab2f951
Elvis <yinchuang94@gmail.com>
2018-10-30 23:39:26 +0800

    fixed navbar errors and added navbar across the apps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,3 +22,33 @@
 //= require react
 //= require react_ujs
 //= require components
+
+$(document).on('turbolinks:load', function(){
+  $("#listing_term").keyup(function(){
+    autocomplete()
+  });
+})
+function autocomplete(){
+  $.ajax({
+    url: '/listings/ajax_search',
+    type: 'GET',
+    data: {query: $("#listing_term").val()}, //takes the form data and authenticity toke and converts it into a standard URL
+    dataType: 'json', //specify what type of data you're expecting back from the servers
+    error: function() {
+        console.log("Something went wrong");
+    },
+    success: function(data) {
+      console.log(data)
+      $("#list").html("");
+
+      data.forEach(function(element) {
+        var option = document.createElement("option");
+        option.value = element;
+        console.log(option)
+
+        //append option to list
+        $("#list").append(option);
+      });
+    }
+  });
+}
